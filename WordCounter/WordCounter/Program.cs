a68cfd7585b150dbed192da8ac44133882306290
Livia Elena Manescu <livia_manescu@yahoo.com>
2020-01-30 17:36:52 -0800

    HomeworkAfterReview

--- WordCounter/WordCounter/Program.cs
+++ WordCounter/WordCounter/Program.cs
@@ -9,39 +9,18 @@ namespace WordCounter
 {
     class Program
     {
-        static int CountWords(String str)
-        {
-            bool state = false;
-            int wordCounter = 0;
-            foreach (int i in str)
-            {
-                if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
-                {
-                    state = false;
-                }
-                else if (state == false)
-                {
-                    state = true;
-                    wordCounter++;
-                }
-            }
-            return wordCounter;
-        }
-
+       
         static void Main(string[] args)
         {
-            //1)first approach(aici ceva nu miroase a bine)
-            //string filePath = @"e:\git\BasicPrograming\WordCounter\WordCounter\bin\Debug\test.txt";
-            //string readedText = File.ReadAllText(filePath);
-            //string[] splitedText = readedText.Split(' ');
-            //Console.WriteLine("The number of words:" + splitedText.Length);
-
-            //2)second approach
-            string path = @"e:\git\BasicPrograming\WordCounter\WordCounter\bin\Debug\test.txt";
-            string readText = File.ReadAllText(path);
-            string[] splitedText = readText.Split(' ');
-            Console.WriteLine("No of words:" + CountWords(readText));
-            
+            string filePath = @"e:\git\BasicProgramming\WordCounter\WordCounter\bin\Debug\test.txt";
+            int numberOfWords = 0;
+            string[] readedText = File.ReadAllLines(filePath);
+            foreach (string line in readedText)
+            {
+                string[] wordsPerLine = line.Split(' ');
+                numberOfWords += wordsPerLine.Length;
+            }
+            Console.WriteLine("Number of words is:" + numberOfWords + Environment.NewLine);
         }
     }
 }
